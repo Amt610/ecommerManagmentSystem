8b4d10cbe1c034ccc65d5afd2d9ee28bda84d861
Amit Yadav <ay3106222@gmail.com>
2026-05-18 18:09:45 -0700

    Initial commit

---
+++ src/main/java/com/example/chatbotapplicatation/ecommermanagmentsystem/EcommerManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatbotapplicatation.ecommermanagmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerManagmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerManagmentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chatbotapplicatation/ecommermanagmentsystem/EcommerManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatbotapplicatation.ecommermanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
