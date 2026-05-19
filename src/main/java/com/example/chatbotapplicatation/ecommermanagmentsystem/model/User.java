e88482b1b559337c87e86c52fdc7037f1ede233e
Amit Yadav <ay3106222@gmail.com>
2026-05-18 18:29:11 -0700

    using table anotation define the name of table "product_table" and update

--- src/main/java/com/example/chatbotapplicatation/ecommermanagmentsystem/model/User.java
+++ src/main/java/com/example/chatbotapplicatation/ecommermanagmentsystem/model/User.java
@@ -1,9 +1,6 @@
 package com.example.chatbotapplicatation.ecommermanagmentsystem.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +9,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "user_table")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
